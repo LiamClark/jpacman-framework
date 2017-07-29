97a4cc5d3b73a457e51e7de43a0a168f044cbf09
rodney clark <rodney.clark@fjorgyn.nl>
2017-07-29 22:34:20 +1000

    Redo ghost random move
    It can no longer return null and now uses higher level vavr operations.

--- src/main/java/nl/tudelft/jpacman/npc/ghost/Ghost.java
+++ src/main/java/nl/tudelft/jpacman/npc/ghost/Ghost.java
@@ -1,19 +1,20 @@
 package nl.tudelft.jpacman.npc.ghost;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
+import io.vavr.collection.Array;
+import io.vavr.control.Try;
 import nl.tudelft.jpacman.board.Direction;
 import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.npc.NPC;
 import nl.tudelft.jpacman.sprite.Sprite;
 
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * An antagonist in the game of Pac-Man, a ghost.
  *
- * @author Jeroen Roosen 
+ * @author Jeroen Roosen
  */
 public abstract class Ghost extends NPC {
 
@@ -35,14 +36,11 @@ public abstract class Ghost extends NPC {
     /**
      * Creates a new ghost.
      *
-     * @param spriteMap
-     *            The sprites for every direction.
-     * @param moveInterval
-     *            The base interval of movement.
-     * @param intervalVariation
-     *            The variation of the interval.
+     * @param spriteMap         The sprites for every direction.
+     * @param moveInterval      The base interval of movement.
+     * @param intervalVariation The variation of the interval.
      */
-    protected Ghost(Square square , Direction direction, Map<Direction, Sprite> spriteMap, int moveInterval, int intervalVariation) {
+    protected Ghost(Square square, Direction direction, Map<Direction, Sprite> spriteMap, int moveInterval, int intervalVariation) {
         super(square, direction);
         this.sprites = spriteMap;
         this.intervalVariation = intervalVariation;
@@ -66,19 +64,13 @@ public abstract class Ghost extends NPC {
      * Determines a possible movedTo in a random direction.
      *
      * @return A direction in which the ghost can movedTo, or <code>null</code> if
-     *         the ghost is shut in by inaccessible squares.
+     * the ghost is shut in by inaccessible squares.
      */
-     protected Direction randomMove() {
-        List<Direction> directions = new ArrayList<>();
-        for (Direction direction : Direction.values()) {
-            if (square.getSquareAt(direction).isAccessibleTo(this)) {
-                directions.add(direction);
-            }
-        }
-        if (directions.isEmpty()) {
-            return null;
-        }
-        int i = new Random().nextInt(directions.size());
-        return directions.get(i);
+    protected Direction randomMove() {
+        Array<Direction> directions = Array.of(Direction.values())
+            .filter(d -> square.getSquareAt(d).isAccessibleTo(this));
+        int i = ThreadLocalRandom.current().nextInt(directions.size());
+        
+        return Try.ofCallable(() -> directions.get(i)).getOrElse(Direction.EAST);
     }
 }
